6015dbd57654ba85c65d995bbc315411436d0fc3
Sander van Rossen <sander.vanrossen@gmail.com>
2024-08-30 15:49:31 +0200

    Added HasMetadataOfType extension to materials

---
+++ Runtime/AssetExtensions/HasMetadataOfTypeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public static class HasMetadataOfTypeExtensions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool HasMetadataOfType<Metadata>(this Material asset)
+        where Metadata : CustomAssetMetadata
+    {
+        return MetadataLookup.HasMetadataOfType<Metadata>(asset);
+    }
+}
